431286ff2e412e0ed9e970eb8254b15f58bf099b
sokunthearithmakara <smakara@hawaii.edu>
2025-02-15 14:51:14 +0700

    Bump version for 1.1.1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_interactivevideo';
-$plugin->release = '1.1.0';
-$plugin->version = 2025021300;
+$plugin->release = '1.1.1';
+$plugin->version = 2025021301;
 $plugin->requires = 2022112800;
 $plugin->supported = [401, 405];
 $plugin->maturity = MATURITY_STABLE;
