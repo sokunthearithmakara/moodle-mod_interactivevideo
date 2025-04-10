8abc4102ca323ada74d40fca59c95c7efd3dd841
sokunthearithmakara <smakara@hawaii.edu>
2025-04-11 05:28:07 +0700

    1.2

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_interactivevideo';
 $plugin->release = '1.2';
-$plugin->version = 2025041100;
+$plugin->version = 2025041101;
 $plugin->requires = 2022112800;
 $plugin->supported = [401, 500];
 $plugin->maturity = MATURITY_STABLE;
